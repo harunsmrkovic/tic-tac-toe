446117312b3764c57242a3639f9a1d4387d22537
Harun Smrkovic <harun@personal.com>
2016-06-03 02:16:41 +0200

    Game engine rendering...

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
-// import vedo from './scripts/game'
+import game from './scripts/game'
 import _ from 'lodash'
+import $ from 'jquery'
 
-const board = [
-    [1, 0, 1],
-    [1, 0, 0],
-    [1, 0, 1]
-  ]
+const tictac = game({ size: 3 })
+
+const letter = { 1: 'X', 2: 'O' }
+
+const render = (board) => {
+  const $board = $('#board')
+
+  return _.flatMap(board, (xa, x) => {
+    return xa.map((player, y) => {
+      const $field = $(`.box[data-x=${x}][data-y=${y}]`)
+      if ($field.length) {
+        $field.text(letter[player])
+      } else {
+        return $board.append(`<div class="box" data-x="${x}" data-y="${y}"></div>`)
+      }
+    })
+  })
+}
+
+let board = tictac.start()
+render(board)
+
+$('#board .box').on('click', function(){
+  board = tictac.move(board, $(this).attr('data-x'), $(this).attr('data-y'), 1)
+  if(tictac.won(board)){
+    console.warn('WON!!!')
+  }
+  render(board)
+})
+
+// field.click =>render(tictac.applyMove)
--- scripts/game.js
+++ scripts/game.js
@@ -1,20 +1,35 @@
-import won from './won'
+import didWon from './won'
 
 const game = ({ size }) => {
-  //
-  // let board = []
-  //
-  // const initBoard = (size) => {
-  //
-  // }
-  //
-  // const start = () => {
-  //   initBoard(size)
-  // }
-  //
-  // return {
-  //   start
-  // }
+
+  const initBoard = () => {
+    return [...Array(size)].map(_ => [...Array(size)].map(_ => 0))
+  }
+
+  const start = () => {
+    return initBoard(size)
+  }
+
+  const move = (board, x, y, player) => {
+    x = parseInt(x)
+    y = parseInt(y)
+
+    if(board[x][y] !== 0) {
+      return board
+    }
+
+    return board.map((xa, nx) => xa.map((val, ny) => (x === nx && y === ny) ? player : val))
+  }
+
+  const won = (board) => {
+    return didWon({ board })
+  }
+
+  return {
+    start,
+    move,
+    won
+  }
 }
 
 export default game
