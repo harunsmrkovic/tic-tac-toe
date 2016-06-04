0dd233354e918ae1a6d0647643a2f22271d2abcc
Harun Smrkovic <harun@personal.com>
2016-06-05 00:14:56 +0200

    Revert "removed obsolete increase score into the move reducer"
    This reverts commit 466a91f2e5f22f4867a7a3cddabeb37421a2ed6c.

--- scripts/mutate.js
+++ scripts/mutate.js
@@ -22,26 +22,20 @@ const mutate = (action, state) => {
       // Can't play already played field
       if (state.board[action.x][action.y] > 0) return state
 
-      // If game was won, do not allow play
-      if(state.won) return state
-
       const board = doMove(state.board, action.x, action.y, state.nowPlaying)
-
-      const won = didWon({ board })
-
-      const x = state.scores && state.scores[1] ? state.scores[1] : 0
-      const o = state.scores && state.scores[2] ? state.scores[2] : 0
 
       return cp(state, {
           board,
-          won,
-          nowPlaying: 1 + Math.abs(state.nowPlaying-2),
-          scores: cp(state.scores, {
-            1: (state.won.winner === 1) ? x+1 : x,
-            2: (state.won.winner === 2) ? o+1 : o
-          })
+          won: didWon({ board }),
+          nowPlaying: 1 + Math.abs(state.nowPlaying-2)
         }
       )
+    case 'INCREASE_SCORE':
+      const x = state.scores && state.scores[1] ? state.scores[1] : 0
+      const o = state.scores && state.scores[2] ? state.scores[2] : 0
+      return cp(state, {
+        scores: { 1: (state.won.winner === 1) ? x+1 : x, 2: (state.won.winner === 2) ? o+1 : o }
+      })
     default:
       return state
   }
