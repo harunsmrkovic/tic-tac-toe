2448412cc97b7c9c1e60d49b8f58a97e6546685e
Harun Smrkovic <harun@personal.com>
2016-06-06 08:39:00 +0200

    bot example folder with es6 imports working

---
+++ bot/index.js
@@ -0,0 +1,1 @@
+import game from '../scripts/game'
--- scripts/mutate.js
+++ scripts/mutate.js
@@ -1,7 +1,7 @@
-import doMove from './move-compiled'
-import didWon from './won-compiled'
-import initBoard from './initboard-compiled'
-import { cp } from './helpers-compiled'
+import doMove from './move'
+import didWon from './won'
+import initBoard from './initboard'
+import { cp } from './helpers'
 
 const mutate = (action, state) => {
   switch(action.type) {
