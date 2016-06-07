0fb4c109942573fa3686de925a95e707147201a5
Faruk Mustafic <fmustafic@infobip.com>
2016-06-07 23:37:23 +0200

    Fix test names.

--- test/bot.js
+++ test/bot.js
@@ -4,21 +4,14 @@ import { assert } from 'chai'
 
 describe('Minimax value checks', function() {
 
-  it('should', function(){
+  it('should recognize potential row win by X', function(){
     assert.deepEqual(
       mmVal([1, 1, 0, 2, 2, 1, 0, 0, 0], 1, 1),
       1
     )
   });
 
-  it('should', function(){
-    assert.deepEqual(
-      mmVal([2, 2, 0, 2, 2, 1, 0, 0, 0], 1, 1),
-      -1
-    )
-  });
-
-  it('should', function(){
+  it('should recognize potential row win by O', function(){
     assert.deepEqual(
       mmVal([2, 2, 0, 2, 2, 1, 0, 0, 0], 1, 1),
       -1
