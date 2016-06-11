ca33fe453b1036b4ff9d62ac3750e2396f0624ff
vedad27 <vedadsose@gmail.com>
2016-06-11 02:42:06 +0200

    Switched X & Y to real coordinates

--- scripts/move.js
+++ scripts/move.js
@@ -2,11 +2,11 @@ const move = (board, x, y, player) => {
   x = parseInt(x)
   y = parseInt(y)
 
-  if(typeof board[x] === 'undefined' || typeof board[x][y] === 'undefined') {
+  if(typeof board[y] === 'undefined' || typeof board[y][x] === 'undefined') {
     return board
   }
 
-  return board.map((xa, nx) => xa.map((val, ny) => (x === nx && y === ny && !val) ? player : val))
+  return board.map((ya, ny) => ya.map((val, nx) => (x === nx && y === ny && !val) ? player : val))
 }
 
 export default move
